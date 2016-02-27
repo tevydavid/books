a193a55191d6abf71375a560d5a13de69bc61ed3
Tevy Jacobs-Gomes <tevydavid@gmail.com>
2016-02-26 19:00:59 -0800

    navbar

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,18 +1,22 @@
 var React = require('react'),
-    ReactDOM = require('react-dom')
-    BooksIndex = require('./booksIndex')
+    ReactDOM = require('react-dom'),
+    BooksIndex = require('./booksIndex'),
+    Navbar = require('./navbar'),
     SponsoredContent = require('./sponsoredContent');
 
 var MainComponent = React.createClass({
   render: function () {
     return(
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-6">
-            <BooksIndex />
-          </div>
-          <div className="col-sm-6">
-            <SponsoredContent />
+      <div>
+        <Navbar/>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6">
+              <BooksIndex />
+            </div>
+            <div className="col-sm-6">
+              <SponsoredContent />
+            </div>
           </div>
         </div>
       </div>
---
+++ frontend/navbar.jsx
@@ -0,0 +1,42 @@
+var React = require('react'),
+    books = require('./books');
+
+var Navbar = React.createClass({
+
+  getInitialState:function(){
+    return({current_books: books.all()})
+  },
+
+  render: function(){
+    var bookList = this.state.current_books.map(function(book, idx){
+      return (
+         <li key={idx} className="button">{book.title}</li>
+       );
+    });
+    
+    bookList.push(<li className="button">Top 10 Australian Beaches</li>);
+
+    return (
+      <div>
+        <div className="navbar-title">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#book-list" aria-expanded="false">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M0 0h24v24h-24z" fill="none"/>
+                <path d="M3 18h18v-2h-18v2zm0-5h18v-2h-18v2zm0-7v2h18v-2h-18z"/>
+            </svg>
+          </button>
+          <p>My Books</p>
+        </div>
+        <div className='container-fluid nav-lower' >
+          <div className="collapse navbar-collapse" id="book-list">
+            <ul className="nav navbar-nav">
+              {bookList}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Navbar;
